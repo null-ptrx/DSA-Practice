a75840a748ce1a1e5d06028e3fc6ed2c389e9734
Yuki-Sn0ww <ds331048@gmail.com>
2026-02-17 22:07:17 +0530

    sorting insertion, bubble, slection

--- 7_sorting/main.cpp
+++ 7_sorting/main.cpp
@@ -1,52 +1,60 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void selection(int arr[], int n)
-{
-    for (int i = 0; i <= n - 2; i++)
-    {
-        int min = i;
-        for (int j = i; j <= n - 1; j++)
-        {
-            if (arr[j] < arr[min])
-            {
-                min = j;
+void selection(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int mini = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[mini]) {
+                mini = j;
             }
         }
-        int temp = arr[min];
-        arr[min] = arr[i];
-        arr[i] = temp;
+
+        swap(arr[i], arr[mini]);
     }
 }
-
-void insertion(int arr[], int n)
-{
-    for (int i = 0; i <= n - 1; i++)
-    {
-        int j = i;
-        while (j > 0 && arr[j - 1] > arr[j])
-        {
-            int temp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = temp;
+void bubble(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
         }
     }
+    
+    for (int i = n - 1; i >= 1; i--) {
+        int didswap = 0;
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                didswap = 1;
+            } 
+        }
+        if (didswap == 0) break;
+    }
+
+
 }
-int main()
-{
+void insertion(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1]) {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+
+}
+int main() {
     int arr[] = {13, 46, 24, 52, 20, 9};
     int n = 6;
-    // cout << "this is unsorted array" << endl;
-    // for (int i = 0; i < n; i++) {
-    //     cout << arr[i] <<" ";
-    // }
-    // cout << endl;
-    selection(arr, n);
-    cout << "this is a sorted array" << endl;
-    for (int i = 0; i < n; i++)
-    {
+
+    bubble(arr, n);
+
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+
     return 0;
 }

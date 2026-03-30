1bba41ecbded72b40c5072943d6d895776f46414
Yuki-Sn0ww <ds331048@gmail.com>
2026-03-30 22:24:08 +0530

    started quick sort - basic concept only

---
+++ new/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+
+    // Read until end of file (important for file input)
+    while (cin >> x) {
+        cout << "Square of " << x << " = " << x * x << endl;
+    }
+
+    return 0;
+}
---
+++ sorting/sorting-II/quick_sort/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void quickSort (int arr[], int low, int high) {
+
+}
+
+int correct(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (i < j && i < high) {
+            if (i < pivot){
+                pivot++;
+            }
+        }
+    }
+    
+}
+int main() {
+    int     
+
+    return 0;
+}

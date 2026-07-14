0c4478d8bfc19256b5de21935ad855f017f7004c
Yuki-Sn0ww <ds331048@gmail.com>
2026-07-14 20:03:11 +0530

    refactor: migrate array algorithm implementations from .cpp files to modular .hpp headers

---
+++ Arrays/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "testCases.hpp"
+#include "Day1/01_Largest_Element.hpp"
+#include "Day2/02_Second_Largest_Element.hpp"
+#include "Day3/03_Check_If_Array_Is_Sorted.hpp"
+#include "Day4/04_Remove_Duplicates_From_Sorted_Array.hpp"
+#include "Day5/05_Left_Rotate_Array_By_One.hpp"
+#include "Day6/06_Left_Rotate_Array_By_K_Places.hpp"
+#include "Day7/07_Move_Zeros_To_End.hpp"
+#include "Day8/08_Linear_Search.hpp"
+#include "Day9/09_Union_Of_Two_Sorted_Arrays.hpp"
+#include "Day10/10_Intersection_Of_Two_Sorted_Arrays.hpp"
+#include "Day11/11_Find_Missing_Number.hpp"
+#include "Day12/12_Maximum_Consecutive_Ones.hpp"
+#include "Day13/13_Find_The_Number_That_Appears_Once.hpp"
+#include "Day14/14_Longest_Subarray_With_Given_Sum_K.hpp"
+#include "Day15/15_Two_Sum.hpp"
+#include "Day16/16_Sort_An_Array_Of_0s_1s_And_2s.hpp"
+#include "Day17/17_Majority_Element.hpp"
+#include "Day18/18_Maximum_Subarray_Sum.hpp"
+#include "Day19/19_Best_Time_To_Buy_And_Sell_Stock.hpp"
+#include "Day20/20_Rearrange_Array_Elements_By_Sign.hpp"
+#include "Day21/21_Next_Permutation.hpp"
+#include "Day22/22_Leaders_In_An_Array.hpp"
+#include "Day23/23_Set_Matrix_Zeroes.hpp"
+#include "Day24/24_Longest_Consecutive_Sequence.hpp"
+
+using namespace std;
+int main() {
+    return 0;
+}
---
+++ Arrays/testCases.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+// ---------- Day01: Move Zeroes ----------
+vector<vector<int>> moveZeroesInputs = {
+    {0,1,0,3,12},
+    {0,0,1},
+    {1,2,3}
+};
+
+// ---------- Day02: Set Matrix Zeroes ----------
+vector<vector<vector<int>>> setMatrixInputs = {
+    {{1,1,1},{1,0,1},{1,1,1}},
+    {{0,1,2,0},{3,4,5,2},{1,3,1,5}}
+};
+
+// ---------- Day03: Longest Consecutive Sequence ----------
+vector<vector<int>> longestConsecutiveInputs = {
+    {100,4,200,1,3,2},
+    {0,3,7,2,5,8,4,6,0,1}
+};
